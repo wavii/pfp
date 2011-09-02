4ca8e3e91b351d6d72f6f397359b3fed3733e4c6
Erik Frey <erik@fawx.com>
2011-09-01 18:52:15 -0700

    0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name='pfp',
-    version='0.0.1',
+    version='0.0.2',
     description='pretty fast statistical parser for natural languages',
     author='Wavii',
     author_email='info@wavii.com',
